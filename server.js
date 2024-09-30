28610156d88ed92803531a997aa95dca45d4a1db
shanebasham <shanebasham28@gmail.com>
2024-09-30 13:37:28 -0600

    server edits

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
+// Index Route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
